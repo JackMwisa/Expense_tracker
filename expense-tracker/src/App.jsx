98a798b65a6818e2d67bc665fad69d3d31db58d2
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2023-10-13 19:32:35 +0300

    styling

--- expense-tracker/src/App.jsx
+++ expense-tracker/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import ExpenseItem from './components/ExpenseItem'
 
 function App() {
    
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
        <h1>Expense Tracker</h1>
+        <ExpenseItem />
     </>
   )
 }
---
+++ expense-tracker/src/components/ExpenseItem.jsx
@@ -0,0 +1,15 @@
+import "./ExpenseItem.css"
+
+function ExpenseItem() {
+    return (
+        <div className="expense-item">
+            <div>Date</div>
+            <div className="expense-item__description">
+                <h2>Car Insurance</h2>
+                <div className="expense-item__price">Amount: $294.67</div>
+            </div>
+        </div>
+    );
+}
+
+export default ExpenseItem;
