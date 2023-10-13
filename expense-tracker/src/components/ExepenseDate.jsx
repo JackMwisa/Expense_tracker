20c7f150e5e328cb98f4c71b44ede75479a73386
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2023-10-13 20:43:09 +0300

    .

--- expense-tracker/src/components/ExepenseDate.jsx
+++ expense-tracker/src/components/ExepenseDate.jsx
@@ -1,4 +1,6 @@
-// date component
+import './ExpenseDate.css'
+
+
 const ExpenseDate = (props) => {
     const month = props.date.toLocaleString('en-US', {month: 'long'});
     const day = props.date.toLocaleString('en-US', {day: '2-digit'});
@@ -7,8 +9,8 @@ const ExpenseDate = (props) => {
     return (
         <div className="expense-date">
             <div className="expense-date__month">{month}</div>
-            <div className="expense-date__day">{day}</div>
             <div className="expense-date__year">{year}</div>
+            <div className="expense-date__day">{day}</div>
         </div>
     )
 
