c4121366cbf93e4eeba953928fd717bbe44205d5
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2023-10-13 20:30:22 +0300

    splitting

--- expense-tracker/src/components/ExpenseItem.jsx
+++ expense-tracker/src/components/ExpenseItem.jsx
@@ -1,27 +1,15 @@
+import ExpenseDate from "./ExepenseDate";
 import "./ExpenseItem.css"
 
 // function ExpenseItem({date, title, amount}) {
 
 function ExpenseItem(props) {
-    // const expenseDate = new Date(2023, 2 ,21);
-    // const expenseTitle = "Car Insurance";
-    // const expenseAmount = 294.67;
-
-    const month = props.date.toLocaleString("USA", {month: 'long'})
-    const day = props.date.toLocaleString("USA", {day: '2-digit'})
-    const year = props.date.getFullYear();
 
     return (
         <div className="expense-item">
-            <div>
-                {/* {props.date.toISOString()} */}
-                
-                <div>{month}</div>
-                <div>{day}</div>
-                <div>{year}</div>
-            
-            
-            </div>
+
+            <ExpenseDate date={props.date} />
+
             <div className="expense-item__description">
                 <h2>{props.title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
