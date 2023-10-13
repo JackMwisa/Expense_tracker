f1c286906d6977d8ad333e33f28752a1293c6ffc
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2023-10-13 19:41:54 +0300

    using variables
    reusability

--- expense-tracker/src/components/ExpenseItem.jsx
+++ expense-tracker/src/components/ExpenseItem.jsx
@@ -1,12 +1,18 @@
 import "./ExpenseItem.css"
 
 function ExpenseItem() {
+
+    const expenseDate = new Date(2023, 2 ,21);
+    const expenseTitle = "Car Insurance";
+    const expenseAmount = 294.67;
+
+
     return (
         <div className="expense-item">
-            <div>Date</div>
+            <div>{expenseDate.toISOString}</div>
             <div className="expense-item__description">
-                <h2>Car Insurance</h2>
-                <div className="expense-item__price">Amount: $294.67</div>
+                <h2>{expenseTitle}</h2>
+                <div className="expense-item__price">${expenseAmount}</div>
             </div>
         </div>
     );
